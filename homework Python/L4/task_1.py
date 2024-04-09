acff2a716730a1e7936d8b3a7029ecada1c73419
DeMonkV87 <zagonenco@gmail.com>
2024-04-10 01:52:36 +0300

    tema pentru 2 lectii

---
+++ homework Python/L4/task_1.py
@@ -0,0 +1,70 @@
+# Aceasta este prima ta sarcină a lecției legată de float în python
+
+# Creează o variabilă numită `age` și seteaz-o la vârsta ta
+
+# CODUL TĂU VINE MAI JOS:
+age=37
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tipul variabilei `age` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(type (age))
+# CODUL TĂU VINE MAI SUS:
+
+# Acum asigură-te că variabila `age` este de tipul float.
+
+# CODUL TĂU VINE MAI JOS:
+print(float(age))
+# CODUL TĂU VINE MAI SUS:
+
+# Creează două variabile numite `age2` și `age3` și setează-le la vârsta prietenilor tăi, ambele variabile trebuie să fie de tipul float.
+
+# CODUL TĂU VINE MAI JOS:
+age2=22.0
+age3=34.5
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează suma vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2+age3)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează diferența vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2-age3)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează produsul vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2*age3)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează împărțirea vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2/age3)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează restul împărțirii vârstelor voastre folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age%age3)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează rezultatul vârstei tale ridicată la puterea vârstei primului prieten folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age**age2)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează rezultatul vârstei primului prieten împărțită la puterea vârstei celui de-al doilea prieten folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age2//age3) #Am scris umpic pe dos, se referă pur și simplu la împărțirea celor două vârste
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
