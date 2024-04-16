de1d0f64d93a9ecf3cdd45b01949bb2e9f8a5cba
DeMonkV87 <zagonenco@gmail.com>
2024-04-17 00:31:28 +0300

    if else elif

---
+++ HW-DeMonkV87/L7/task_1.py
@@ -0,0 +1,106 @@
+# Aceasta este sarcina pentru lecția 7 legată de comentarii, continuarea liniilor și instrucțiuni condiționale.
+
+# Creați o variabilă numită number și atribuiți-i o valoare întreagă.
+
+# CODUL TĂU VINE MAI JOS:
+number = 101
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă numărul este pozitiv și afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if number > 0:
+             print ("numarul este pozitiv")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă numărul este par și afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if number % 2 == 0:
+             print("numar par")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă numărul este impar și afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if number % 2 == 1:
+             print("numarul este impar")
+# CODUL TĂU VINE MAI SUS:
+
+# Creați o variabilă text și atribuiți-i un șir de caractere.
+
+# CODUL TĂU VINE MAI JOS:
+text = ("luni", "marti", "soare",)
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if "Python" in text:
+             print("cuvintul 'Python' este in text")
+else:
+             print ("cuvintul 'Python' nu este in text")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Java" și afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if ("Java") in text:
+             print("cuvintul 'Java' este in text")
+else:
+             print ("cuvintul 'Java' nu este in text")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
+# în cazul în care nu conține, verificați dacă conține cuvântul "Java" și afișați un mesaj corespunzător.
+# Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if ("Python") in text:
+             print("cuvintul 'Python' este in text")
+else :
+             print ("cuvintul 'Python' nu este in text")
+if "Java" in text:
+             print("cuvintul 'Java' este in text")
+else:
+             print ("cuvintul 'Java' nu este in text")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și cuvântul "Java" și afișați un mesaj corespunzător.
+# În cazul în care conține doar unul dintre ele, afișați un mesaj corespunzător.
+# Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
+
+# CODUL TĂU VINE MAI JOS:
+if ("Python") in text and ("Java") in text:
+        print("Ambele cuvinte sunt in text")
+elif ("Python") in text:
+        print("Cuvintul 'Python' este in text")
+elif ("Java") in text:
+         print("Cuvintul 'Java' este in text")
+else:
+        print("Asa cuvinte nu sunt in text")
+# CODUL TĂU VINE MAI SUS:
+
+# Extrageți de la tastatură utilizând funcția input un număr de la 1 la 5 și atribuiți-l variabilei number.
+# Folosiți o instrucțiune condițională pentru a printa un mesaj corespunzător în funcție de numărul introdus.
+# pentru 1 - printați "Unu"
+# pentru 2 - printați "Doi"
+# pentru 3 - printați "Trei"
+# pentru 4 - printați "Patru"
+# pentru 5 - printați "Cinci"
+
+# CODUL TĂU VINE MAI JOS:
+x = input ("Scrie un numar de la 1 la 5:")
+if x == "1": 
+             print("Unu")
+elif x == "2": 
+             print("Doi")
+elif x == "3": 
+             print("Trei")
+elif x == "4": 
+             print("Patru")
+elif x == "5": 
+             print("Cinci")
+# CODUL TĂU VINE MAI SUS:
+
+
