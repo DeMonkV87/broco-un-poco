b6137f4c44c0b2bdafcdf07209fc7324ad2af219
DeMonkV87 <zagonenco@gmail.com>
2024-04-24 11:38:07 +0300

    last

--- HW-DeMonkV87/L8/task_1.py
+++ HW-DeMonkV87/L8/task_1.py
@@ -77,12 +77,9 @@ print(listaX)
 
 # CODUL TĂU VINE MAI JOS:
 listaY = [1, 2, 3, 4, 5]
-
-# while i in lisaY <= 50:
-#      for i, element in enumerate(listaY):
-#           if (element) % 50:
-#                listaY[i] = str(int(element)*2)
-#           print(listaY)
+while max(listaY) <=50:
+    listaY = [i*2 for i in listaY]
+    print(listaY)
 # CODUL TĂU VINE MAI SUS:
 
 # Generează și printează o listă cu toate numerele pătrat perfect din intervalul [1, 100].
@@ -124,12 +121,17 @@ for sublist in lista_de_liste:
 # Folosind un buclă while , caută și printează prima valoare care este mai mare decât 10 dintr-o listă cu numere random creată de tine. Dacă nu există, printează "Nu există valori mai mari decât 10".
 
 # CODUL TĂU VINE MAI JOS:
-random_list = [1, 3, 5, 7, 9, 7, 12, 21]
-while i in enumerate(random_list):
-     if (i in random_list) < 10:
-          print(i)
-     else:
-          print("Nu există valori mai mari decât 10")
+random_list = [1, 3, 5, 7, 9, 7, 10, 21]
+num_de_comparatie = False
+x = 0
+while x < len(random_list):
+    if random_list[x]> 10:
+        print(random_list[x])
+        num_de_comparatie = True
+        break
+    x +=1
+else:
+    print("Nu există valori mai mari decât 10 în lista generată.")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind loop-uri Creează un pătrat de stele ( * ) folosind bucle încadrate. Dimensiunea pătratului va fi citită de la utilizator.
@@ -234,11 +236,12 @@ for i in range(1, 4+1):
 # 243
 
 # CODUL TĂU VINE MAI JOS:
-# TreilaPutere  = ()
-# for i in range(i,i**2):
-#     for j in range(1,5):
-#         print("3"'\n', end="")
-
+TreilaPutere  = [3**i for i in range(1,6,1)]
+for i in range(len(TreilaPutere)):
+    for j in range(i+1):
+        print(TreilaPutere[j], end= " ")
+    print()
+#  mai e nevoie sa scriu pentru descrestere
 # CODUL TĂU VINE MAI SUS:
 
 # Completați sarcinile de mai sus pentru a exersa lucrul cu buclele în Python.
